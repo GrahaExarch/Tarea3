24a0d85dfa9a29e5d039fa4d8c9613001edaac02
jorgefer77 <82996206+jorgefer77@users.noreply.github.com>
2022-11-22 01:22:24 -0300

    Add files via upload

---
+++ sqlite.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+conn = sqlite3.connect('BD.db')
+c = conn.cursor()
+
+c.execute(""" CREATE TABLE IF NOT EXISTS Admin (
+    Username TEXT NOT NULL,
+    Password TEXT NOT NULL) """)
+
+c.execute(""" CREATE TABLE IF NOT EXISTS Company (
+    ID INTEGER PRIMARY KEY,
+    company_name TEXT NOT NULL,
+    company_api_key TEXT NOT NULL) """)
+
+c.execute(""" CREATE TABLE IF NOT EXISTS Location (
+    company_id INTEGER PRIMARY KEY,
+    location_name TEXT NOT NULL,
+    location_country TEXT NOT NULL,
+    location_city TEXT NOT NULL,
+    location_meta TEXT NOT NULL) """)
+
+c.execute(""" CREATE TABLE IF NOT EXISTS Sensor (
+    location_id INTEGER PRIMARY KEY,
+    sensor_id Integer NOT NULL,
+    sensor_name TEXT NOT NULL,
+    sensor_category TEXT NOT NULL,
+    sensor_meta TEXT NOT NULL,
+    sensor_api_key TEXT NOT NULL) """)
+
+
+conn.commit()
+conn.close()
+
+
+
+    
+
+
+
+
+
