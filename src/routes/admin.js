0af0f6830f0797b4961138bdfa5d1db1dfaee292
Javier Ramos <javier.ramos1@mail.udp.cl>
2022-12-04 16:26:17 -0300

    Se terminan los metodos de admin y se migran los de location

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -18,16 +18,29 @@ router.get('/', async (req,res) => {
   });
 });
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
+router.get('/:username', async (req, res) => {
+  const username = req.params.username;
   let sql = 'select * from admin where username = ?';
-  db.all(sql, [id], (err, rows)=>{
+  await db.all(sql, [username], (err, rows)=>{
     if(err) {
       res.status(500).json({"error":err.message});
     }
     res.json({
         datos: rows,
     });
+  });
+});
+
+router.post('/', async(req, res) => {
+  const { username, password } = req.body;
+  const sql = 'INSERT INTO admin VALUES (?,?)';
+  await db.run(sql, [username, password], err => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
+      }
+      res.json({
+          "response": 'Usuario insertado'
+      });
   });
 });
 
@@ -45,15 +58,62 @@ router.post('/create_company', async (req, res) => {
   });
 });
 
-router.put('/:id', (req, res) => {
-const id = req.params.id;
-res.json({
-  "response": 'estas viendo post con un id',
-  "id": id
-})
+router.post('/create_location', async (req, res) => {
+  const {id, location_name, location_country, location_city, location_meta} = req.body;
+  const sql = 'INSERT INTO location VALUES (?,?,?)';
+  await db.run(sql, [id, location_name, location_country, location_city, location_meta], err =>{
+    if(err){
+      res.status(500).json({ "error": err.message });
+    }
+    res.json({
+      "response": 'Insertada compañia'
+    });
+  });
 });
 
+router.post('/create_sensor', async (req, res) => {
+  const {location_id, sensor_id, sensor_name,sensor_category, sensor_meta} = req.body;
+  const key = uuidv4();
+  const sql = 'INSERT INTO sensor VALUES (?,?,?,?,?,?)';
+  await db.run(sql, [location_id, sensor_id, sensor_name,sensor_category, sensor_meta, key], err =>{
+    if(err){
+      res.status(500).json({ "error": err.message });
+    }
+    res.json({
+      "response": 'Insertado sensor'
+    });
+  });
+});
 
+router.put('/:username', async (req, res) => {
+  const username = req.params.username;
+  const new_username = req.body.username;
+  const password = req.body.password;
+  const sql = 'update admin set username = ?, password = ? where username = ?'
+  await db.run(sql, [new_username, password, username], err => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
+      }
+      res.json({
+          "response": 'User updated'
+      });
+  });
+});
+
+router.delete('/:username', async (req, res) => {
+  const username = req.params.username;
+  const password = req.body.password;
+  const sql = 'delete from admin where username = ? and password = ?'
+  console.log(username,password)
+  await db.run(sql, [username,password], err => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
+      }
+      res.json({
+          "response": 'Usuario eliminado'
+      });
+  });
+});
 
 
 module.exports = router;
--- src/routes/location.js
+++ src/routes/location.js
@@ -2,48 +2,70 @@ const { Router} = require('express');
 const router = Router();
 const db = require('../db');
 
-router.get('/', (req, res) => {
-  res.json({"titulo": "estas viendo  get"});
-});
-
-router.get('/all', (req, res) => {
-  let sql = 'SELECT * FROM location'
-  db.serialize(()=>{
-    db.all(sql, (err,rows)=>{
-      if(err){
-        res.status(500).json({"error":err.message})
+router.get('/:location_name', async (req, res) => {
+  const location_name = req.params.location_name
+  const sql = 'select * FROM location where location_name = ?';
+  await db.all(sql, [location_name], (err, rows) => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
       }
       res.json({
-        datos:rows,
+          datos: rows,
       });
-    });
   });
 });
 
-router.post('/', (req, res) => {
-  const data = req.body;
-  if (data) {
-      res.json({"response": "estas viendo post"})
-  }
-  else {
-      res.status(500).json({ "error": "There was an error." });
-  }
+router.get('/', async(req, res) => {
+  const sql = 'select * FROM location';
+  await db.all(sql, (err, rows) => {
+      if (err) {
+          res.status(500).json({ "error": err.message })
+      }
+      console.log(rows);
+      res.json({
+          datos: rows,
+      });
+  });
 });
 
-router.put('/:id', (req, res) => {
-const id = req.params.id;
-res.json({
-  "response": 'estas viendo post con un id',
-  "id": id
-})
+router.post('/', async(req, res) => {
+  const { company_id, location_name, location_country, location_city, location_meta } = req.query;
+  const sql = 'INSERT INTO location VALUES (?,?,?,?,?)';
+  await db.run(sql, [company_id, location_name, location_country, location_city, location_meta], err => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
+      }
+      res.json({
+          "response": 'Lugar insertado'
+      });
+  });
 });
 
-router.delete('/:id', (req, res) => {
-const id = req.params.id;
-res.json({
-  "response": 'estas viendo delete',
-  "id": id
+router.put('/:id', async (req, res) => {
+  const company_id = req.params.company_id;
+  const location_name = req.query.location_name;
+  const sql = 'update company set location_name = ? where company_id = ?'
+  await db.run(sql, [location_name, company_id], err => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
+      }
+      res.json({
+          "response": 'Location updated'
+      });
+  });
 });
+
+router.delete('/', async (req, res) => {
+  const location_name = req.query.location_name;
+  const sql = 'delete from location where location_name = ?'
+  await db.run(sql, [location_name], err => {
+      if (err) {
+          res.status(500).json({ "error": err.message });
+      }
+      res.json({
+          "response": 'Lugar eliminado'
+      });
+  });
 });
 
 module.exports =router;
